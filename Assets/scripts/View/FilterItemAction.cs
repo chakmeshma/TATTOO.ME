891276715d048a36a09f73a9b2b01d4b3f477b69
Majid Motallebikashani <chakmeshma@gmail.com>
2019-03-05 18:27:14 +0330

    Top bar, Title Bar, Dropdown

---
+++ Assets/scripts/View/FilterItemAction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FilterItemAction : MonoBehaviour
+{
+    private Image thisImage;
+    private Text thisText;
+    private bool selected = false;
+
+    private void Awake()
+    {
+        thisImage = GetComponentInChildren<Image>();
+        thisText = GetComponentInChildren<Text>();
+    }
+
+    private void OnMouseDown()
+    {
+        selected = !selected;
+
+        updateView();
+    }
+
+    private void updateView()
+    {
+        if(selected)
+        {
+            thisImage.color = Color.black;
+            thisText.color = Color.white;
+        }
+        else
+        {
+            thisImage.color = Color.white;
+            thisText.color = Color.black;
+        }
+    }
+}
